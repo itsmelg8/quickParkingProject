2bab753e6f782ec74b812563bc40274beb08a70e
Humberto Campos <138628317+Humbertohccampos@users.noreply.github.com>
2023-09-16 15:45:56 -0300

    Update rotas.js

--- src/rotas.js
+++ src/rotas.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const cadastroUsuario = require('./controladores/cadastrarUsuario.js');
 const login = require('./controladores/login.js');
+const cadastrarVeiculo = require('./controladores/cadastroVeiculo.js');
 
 const rotas = express();
 
 rotas.post('/cadastro', cadastroUsuario);
 rotas.post('/login', login);
+rotas.post('/cadastroVeiculo', cadastrarVeiculo);
 
 module.exports = rotas;
